cb3a97b5dc4bf434602982bdce1cb53dc15b1c8d
Yoryi Harmony <tharealjharmony@gmail.com>
2022-11-21 19:50:39 -0500

    initial commit

--- emoji-pop.js
+++ emoji-pop.js
@@ -5,7 +5,7 @@ class Fountain {
     this.autoAddParticle = false;
     this.height = document.documentElement.clientHeight;
     this.sizes = [15, 20, 25, 35, 45];
-    this.variants = ["🧟", "🧟"];
+    this.variants = ["💈", "💈"];
     this.addHandlers();
     this.loop();
   }
